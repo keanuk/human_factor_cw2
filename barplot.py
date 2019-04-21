ba8fcc0d4b08bc53eb568d04a1499f80581cb65c
Keanu Kerr <keanukerr@gmail.com>
2019-04-21 01:30:13 +0100

    Update barplot.py

--- barplot.py
+++ barplot.py
@@ -1,49 +1,29 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 import csv
 
-sns.set(style="white", context="talk")
+# plt.rcParams["axes.labelsize"] = 30
+# sns.set(font_scale=10)
+# sns.set_context("paper", rc={"axes.labelsize":72})
 
-with open('airline-safety.csv') as csvData:
-    airData = csv.reader(csvData, delimiter=',')
-    airline = []
-    km = []
-    inc84 = []
-    fatinc84 = []
-    fat84 = []
-    inc00 = []
-    fatinc00 = []
-    fat00 = []
-    next(airData)
-    for row in airData:
-        airline.append(row[0])
-        km.append(row[1])
-        inc84.append(row[2])
-        fatinc84.append(row[3])
-        fat84.append(row[4])
-        inc00.append(row[5])
-        fatinc00.append(row[6])
-        fat00.append(row[7])
+airData = pd.read_csv('airline-safety.csv')
+airData = airData.sort_values('avail_seat_km_per_week', ascending=False)
+# print(airData)
+airData['total_fatalities'] = airData['fatalities_85_99'] + airData['fatalities_00_14']
 
-# Set up the matplotlib figure
-f, ax = plt.subplots(figsize=(7, 5))
+sns.set(style="whitegrid")
+f, ax = plt.subplots(figsize=(25, 18))
 
-fats = []
+sns.set_color_codes("pastel")
+sns.barplot(x='total_fatalities', y='airline', data=airData[0:19].sort_values('total_fatalities', ascending=False), label="2000-2014", color="#aed7ff", ax=ax)
+sns.set_color_codes("muted")
+sns.barplot(x='fatalities_85_99', y='airline', data=airData[0:19].sort_values('total_fatalities', ascending=False), label="1984-1999", color="#4aa5ff", ax=ax)
 
-for i, j in zip(fat84, fat00):
-    fats.append(int(j) - int(i))
-
-# Center the data to make it diverging
-x = airline
-y = fats
-sns.barplot(x=x, y=y, palette="vlag", ax=ax)
-ax.axhline(0, color="k", clip_on=False)
-ax.set_ylabel("Safety Improvement")
-
-# Finalize the plot
-sns.despine(bottom=True)
-plt.setp(f.axes, yticks=[])
-plt.tight_layout(h_pad=2)
+sns.set(style="white", context="talk", font_scale=2)
+ax.set(xlim=(0, 600), ylabel="Airlines", xlabel="Fatalities")
+ax.legend(ncol=2, loc="lower right", frameon=True)
+sns.despine(left=True, bottom=True)
 
 plt.show()
